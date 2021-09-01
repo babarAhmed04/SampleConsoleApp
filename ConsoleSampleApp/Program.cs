62d02171a9e40059b31fc84194598a6f99483776
Babar Ahmed <babar_ahmed@dell.com>
2021-09-01 12:46:58 +0530

    Add project files.

---
+++ ConsoleSampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sample Project");
+            Console.ReadKey();
+        }
+    }
+}
